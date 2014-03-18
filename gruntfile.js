0c82a90f0d54987c1abcfb08a1bf25e6b38ddfc9
Qazzian <Qazzian@gmail.com>
2014-03-18 10:48:10 +0000

    Started The re-scripting process.

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,19 @@
+var fs = require('fs');
+
 module.exports = function(grunt){
+
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+
+        less: {
+            config: {
+            },
+            files: {
+                'styles.css': fs.join('templates', 'assets', 'less', 'manifest.less')
+            }
+        }
     });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.registerTask('default', ['less']);
 };
