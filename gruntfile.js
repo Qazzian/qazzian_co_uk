c129ac771f44fdc481460c9c73097f821acff427
Qazzian <Qazzian@gmail.com>
2014-03-26 11:57:13 +0000

    Got some js deps and less files building

--- gruntfile.js
+++ gruntfile.js
@@ -1,19 +1,75 @@
 var fs = require('fs');
 
-module.exports = function(grunt){
+module.exports = function (grunt) {
+	"use strict";
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 
-        less: {
-            config: {
-            },
-            files: {
-                'styles.css': fs.join('templates', 'assets', 'less', 'manifest.less')
-            }
-        }
-    });
+		clean: ['dist', 'build'],
 
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.registerTask('default', ['less']);
+		copy: {
+			bowerDeps: {
+				config: {
+					encoding: 'utf8',
+					filter: 'isFile'
+				},
+				files: [
+					{
+						expand: true,
+						flatten: true,
+						cwd: 'bower_components/jquery/dist/',
+						src: ['jquery.min.*'],
+						dest: 'dist/assets/js/lib/'
+					},
+					{
+						expand: true,
+						flatten: true,
+						cwd: 'bower_components/bootstrap/dist/',
+						src: ['bootstrap.min.js*'],
+						dest: 'dist/assets/js/lib/'
+					}
+				]
+			},
+			assets: {
+				config: {
+					encoding: 'utf8',
+					filter: 'isFile'
+				},
+				files: [
+					{
+						expand: true,
+						cwd: 'templates/',
+						src: 'assets/**',
+						dest: 'dist'
+					}
+				]
+			}
+
+		},
+
+		less: {
+			dev: {
+				options: {
+					paths: ["bower_components/bootstrap/less/"]
+				},
+				files: {
+					'dist/assets/css/manifest.css': 'templates/assets/less/manifest.less'
+				}
+			}
+		}
+
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-less');
+
+	grunt.loadTasks('grunt/bower-deps.js');
+
+
+	grunt.registerTask('collectDeps', ['copy']);
+
+	grunt.registerTask('default', ['clean', 'collectDeps', 'less']);
 };
