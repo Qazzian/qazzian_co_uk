6cda97536f560d3a30161b31a0b9fef4a6acc4c6
Qazzian <Qazzian@gmail.com>
2014-03-28 12:26:50 +0000

    Clean up the dependencies

--- gruntfile.js
+++ gruntfile.js
@@ -62,14 +62,25 @@ module.exports = function (grunt) {
 
 	});
 
+	grunt.registerTask('bowerInstall', 'install the frontend dependencies', function() {
+		// adapted from http://www.dzone.com/snippets/execute-unix-command-nodejs
+		var exec = require('child_process').exec,
+				sys  = require('sys');
+
+		function puts(error, stdout, stderr) { console.log(stdout); sys.puts(stdout) }
+
+		// assuming this command is run from the root of the repo
+		exec('bower install', puts);
+	});
+
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-less');
 
 	grunt.loadTasks('grunt');
 
+	grunt.registerTask('default', 'Run as part of normal development of the site templates', ['clean', 'copy', 'less', 'cmsBuildHtml']);
+	grunt.registerTask('install', 'Run when first installing this cms.', ['bowerInstall', 'copy', 'less', 'cmsBuildHtml']);
+	grunt.registerTask('update', 'Run if any of the bower dependencies change.', ['clean', 'bowerInstall', 'copy', 'less', 'cmsBuildHtml']);
 
-	grunt.registerTask('collectDeps', ['copy']);
-
-	grunt.registerTask('default', ['clean', 'collectDeps', 'less', 'cmsBuildHtml']);
 };
